a84afc291128566a4a6b5243c9b720d5f649bed6
soukagorko <139471801+soukagorko@users.noreply.github.com>
2023-07-13 21:44:24 +0000

    Add files via upload

---
+++ src/main/java/com/spring_gestion_scolaire/SpringGestionScolaireApplication.java
@@ -0,0 +1,13 @@
+package com.spring_gestion_scolaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGestionScolaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGestionScolaireApplication.class, args);
+	}
+
+}
